b86f525ac3c3abbd6fcf775c4b9563dcb4865c8f
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2022-05-20 12:23:53 +0300

    mobile js update

--- script/main.js
+++ script/main.js
@@ -1,13 +1,14 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
-
 const hamburgerButton = document.getElementById('hamburgerButton');
-
 const mobileLinks = document.getElementById('mobileLinks');
 
 hamburgerButton.addEventListener('click',function showMenu(){
         hamburgerButton.classList.toggle('change')
         mobileLinks.classList.toggle('hide');
 })
+
+// const toggleButton = document.getElementByIdU('check-price');
+
+// toggleButton.addEventListener('mouseenter',function(){
+//     alert(1)
+// })
 
